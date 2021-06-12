2e5212f0211f307566da41c74ac336ab27b57719
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-06-12 10:07:36 -0700

    set up files

--- server.js
+++ server.js
@@ -2,15 +2,34 @@ const express = require('express');
 const exphb = require('express-handlebars');
 const hbs = exphb.create({});
 const path = require('path');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/config.js')
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sesh = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/routes'));
 
 app.listen(PORT, () => {
     console.log('Server listening on  http://localhost:' + PORT);
+    sequelize.sync({ force: false });
 });

2c9d59bb63d4345ffb708d22bae643d3e3c70999
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-06-16 11:34:17 -0700

    postroutes

---
+++ controllers/api/postroutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {Post, Comment, User} = require('../models');
+const sequelize = require('../../config/config');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'content'],
+        include: [
+            {model: User, attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }]
+    }).then(dbPostData => res.json(dbPostData.reverse()))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+

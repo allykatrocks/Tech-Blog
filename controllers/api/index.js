bd0767a06c90057ee028e4b7db9d6cacc2bb4672
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-06-26 12:57:25 -0700

    added comment route to api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userroutes = require('./userroutes');
 const postroutes = require('./postroutes');
+const commentroutes = require('./commentroutes');
 
 router.use('/users', userroutes);
 router.use('/posts', postroutes);
+router.use('/comment', commentroutes);
 
 module.exports = router;
